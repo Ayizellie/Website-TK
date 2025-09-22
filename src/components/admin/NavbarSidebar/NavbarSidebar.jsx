1b0e6d6fc320b3d5ca78dfa0ef4ffbfbad963af0
nasyaimutdanlucu <nasyaadea@gmail.com>
2025-09-22 21:34:59 +0800

    baikin sidebar

--- src/components/admin/NavbarSidebar/NavbarSidebar.jsx
+++ src/components/admin/NavbarSidebar/NavbarSidebar.jsx
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavbarAdmin from '../navbaradmin/NavbarAdmin';
 import Sidebar from '../sidebaradmin/Sidebar';
 
 const NavbarSidebar = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [
+    isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  useEffect(() => {
+    const savedState = localStorage.getItem('sidebarbuka');
+    if (savedState !== null){
+      setIsSidebarOpen(savedState === 'true');
+    } 
+  }, []);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen(prev => !prev);
+    setIsSidebarOpen(prev => {
+      const stateBaru = !prev;
+      localStorage.setItem('sidebarbuka', stateBaru.toString());
+      return stateBaru;
+    });
   };
 
   return (
     <div className="flex h-screen w-full">
-      <Sidebar isSidebarOpen={isSidebarOpen} />
-
+      <Sidebar 
+      isSidebarOpen={isSidebarOpen} />
       <div className="flex flex-col flex-1">
-        {isSidebarOpen ? (
-          <div className="w-full">
-            <NavbarAdmin toggleSidebar={toggleSidebar} />
-          </div>
-        ) : (
-          <div className="w-full">
-            <NavbarAdmin toggleSidebar={toggleSidebar} />
-          </div>
-        )}
-
+        <NavbarAdmin 
+        toggleSidebar={toggleSidebar} />
         <main className="flex-1 p-4 bg-[#f8f8f8] overflow-y-auto">
           {children}
         </main>
